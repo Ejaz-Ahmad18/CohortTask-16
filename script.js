0ce2ee35532b7da0624765367407001d28ddf344
ejazahmad18 <numen.ejaz@gmail.com>
2025-11-24 16:02:31 +0530

    Using-ClearIntervl

--- script.js
+++ script.js
@@ -2,9 +2,24 @@ var h1=document.querySelector('h1')
 var btn=document.querySelector('button')
 
 
-btn.addEventListener('click', function(){
-    h1.innerHTML='Changing user'
-  setTimeout(function(){
-    h1.innerHTML=' Hello I am Ahmad'
-  },2000)
-})
+// btn.addEventListener('click', function(){
+//     h1.innerHTML='Changing user'
+//   setTimeout(function(){
+//     h1.innerHTML=' Hello I am Ahmad'
+//   },2000)
+// })
+
+let a=0
+
+ var b= setInterval(function(){
+    console.log(a)
+    a++
+  },100)
+
+  setTimeout(()=>{
+clearInterval(b)
+
+},5000)
+  
+
+
