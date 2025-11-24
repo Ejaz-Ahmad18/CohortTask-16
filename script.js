0197a38c755a67dc4f48f668dec2bec0760debf1
ejazahmad18 <numen.ejaz@gmail.com>
2025-11-24 16:08:54 +0530

    USING-ALL- IN -addEventListener

--- script.js
+++ script.js
@@ -9,10 +9,23 @@ var btn=document.querySelector('button')
 //   },2000)
 // })
 
-let a=0
+// let a=0
 
- var b= setInterval(function(){
-    console.log(a)
+//  var b= setInterval(function(){
+//     console.log(a)
+//     a++
+//   },100)
+
+//   setTimeout(()=>{
+// clearInterval(b)
+
+// },5000)
+
+let a=0
+btn.addEventListener('click', function(){
+    
+   var b= setInterval(function(){
+    h1.innerHTML=(a)
     a++
   },100)
 
@@ -20,6 +33,10 @@ let a=0
 clearInterval(b)
 
 },5000)
+})
+
+  
+
   
 
 
